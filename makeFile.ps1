a3535ee3e187ffd44f473b97aa5cfaa10b35e6c1
Geryones <the.jurij@gmail.com>
2019-11-01 11:08:28 +0100

    init

---
+++ makeFile.ps1
@@ -0,0 +1,27 @@
+$dir = Get-Location 
+set-Location $dir
+$exclude = @("001-abstract.md", "005-zusammenfassung.md")
+$input = Get-Item "src\*.md" -Exclude $exclude | Resolve-Path -Relative
+
+
+
+
+pandoc $input -o out/Report.pdf `
+    --include-before-body=".\src\001-abstract.md" `
+    --include-before-body=".\src\005-zusammenfassung.md" `
+    --metadata-file=metadata.yml `
+    --from markdown `
+    --template="./templates/eisvogel.latex" `
+    --listings `
+    --number-sections `
+    -V lang=de `
+    -V papersize:a4 `
+    -V book `
+    -V classoption=oneside `
+    --toc `
+    --toc-depth=2 `
+    --bibliography=biblio.bib  `
+    --csl=ieee.csl `
+    --highlight-style breezedark `
+    --top-level-division=chapter `
+    --filter pandoc-citeproc 
